7424516857f518f48b38514d7ad42deed7a656b9
dwalend <david@walend.net>
2014-04-08 16:50:36 -0400

    Got OneShortestPathTest to be consistent

--- src/test/scala/walend/scalax/semiring/OneShortestPathTest.scala
+++ src/test/scala/walend/scalax/semiring/OneShortestPathTest.scala
@@ -57,6 +57,48 @@ class OneShortestPathTest extends FlatSpec with Matchers {
     (A~+>H)(Some(Step(5,Some(B)))),
     (A~+>F)(Some(Step(5,Some(B)))))
 
+  val expectedEdges2 = Set(
+    (B~+>B)(Some(Step(0,None))),
+    (B~+>F)(Some(Step(4,Some(C)))),
+    (B~+>C)(Some(Step(1,Some(C)))),
+    (B~+>D)(Some(Step(2,Some(C)))),
+    (B~+>H)(Some(Step(4,Some(C)))),
+    (B~+>E)(Some(Step(3,Some(C)))),
+    (F~+>F)(Some(Step(0,None))),
+    (C~+>B)(Some(Step(3,Some(D)))),
+    (C~+>F)(Some(Step(3,Some(D)))),
+    (C~+>C)(Some(Step(0,None))),
+    (C~+>D)(Some(Step(1,Some(D)))),
+    (C~+>H)(Some(Step(3,Some(D)))),
+    (C~+>E)(Some(Step(2,Some(D)))),
+    (G~+>G)(Some(Step(0,None))),
+    (D~+>B)(Some(Step(2,Some(E)))),
+    (D~+>F)(Some(Step(2,Some(E)))),
+    (D~+>C)(Some(Step(3,Some(E)))),
+    (D~+>D)(Some(Step(0,None))),
+    (D~+>H)(Some(Step(2,Some(E)))),
+    (D~+>E)(Some(Step(1,Some(E)))),
+    (H~+>B)(Some(Step(4,Some(C)))),
+    (H~+>F)(Some(Step(4,Some(C)))),
+    (H~+>C)(Some(Step(1,Some(C)))),
+    (H~+>D)(Some(Step(2,Some(C)))),
+    (H~+>H)(Some(Step(0,None))),
+    (H~+>E)(Some(Step(3,Some(C)))),
+    (E~+>B)(Some(Step(1,Some(B)))),
+    (E~+>F)(Some(Step(1,Some(F)))),
+    (E~+>C)(Some(Step(2,Some(B)))),
+    (E~+>D)(Some(Step(3,Some(B)))),
+    (E~+>H)(Some(Step(1,Some(H)))),
+    (E~+>E)(Some(Step(0,None))),
+    (A~+>B)(Some(Step(1,Some(B)))),
+    (A~+>F)(Some(Step(5,Some(B)))),
+    (A~+>C)(Some(Step(2,Some(B)))),
+    (A~+>D)(Some(Step(3,Some(B)))),
+    (A~+>H)(Some(Step(5,Some(B)))),
+    (A~+>E)(Some(Step(4,Some(B)))),
+    (A~+>A)(Some(Step(0,None)))
+  )
+
   "Initializing the label graph" should "produce a label graph with self-edges and edges where SomeGraph has them" in {
 
     val labelGraph = new OneShortestPathGraphBuilder[String](oneShortestPath.semiring).initialLabelGraph(testGraph)
@@ -81,7 +123,7 @@ class OneShortestPathTest extends FlatSpec with Matchers {
     )
 
     labelGraph.edges.toOuter should be (expectedEdges)
-    EdgeHelp.checkEdgeSets(labelGraph.edges.toOuter.asInstanceOf[Set[MLDiEdge[String]]],expectedEdges) should be (true)
+    EdgeHelp.checkEdgeSets(labelGraph.edges.toOuter.asInstanceOf[Set[MLDiEdge[String]]],expectedEdges) should be (None)
 
   }
 
@@ -107,50 +149,8 @@ class OneShortestPathTest extends FlatSpec with Matchers {
 
     val labelGraph = FloydWarshall.allPairsShortestPaths(oneShortestPath.semiring,new OneShortestPathGraphBuilder[String](oneShortestPath.semiring))(graph)
 
-    val expectedEdges2 = Set(
-      (B~+>B)(Some(Step(0,None))),
-      (B~+>F)(Some(Step(4,Some(C)))),
-      (B~+>C)(Some(Step(1,Some(C)))),
-      (B~+>D)(Some(Step(2,Some(C)))),
-      (B~+>H)(Some(Step(4,Some(C)))),
-      (B~+>E)(Some(Step(3,Some(C)))),
-      (F~+>F)(Some(Step(0,None))),
-      (C~+>B)(Some(Step(3,Some(D)))),
-      (C~+>F)(Some(Step(3,Some(D)))),
-      (C~+>C)(Some(Step(0,None))),
-      (C~+>D)(Some(Step(1,Some(D)))),
-      (C~+>H)(Some(Step(3,Some(D)))),
-      (C~+>E)(Some(Step(2,Some(D)))),
-      (G~+>G)(Some(Step(0,None))),
-      (D~+>B)(Some(Step(2,Some(E)))),
-      (D~+>F)(Some(Step(2,Some(E)))),
-      (D~+>C)(Some(Step(3,Some(E)))),
-      (D~+>D)(Some(Step(0,None))),
-      (D~+>H)(Some(Step(2,Some(E)))),
-      (D~+>E)(Some(Step(1,Some(E)))),
-      (H~+>B)(Some(Step(4,Some(C)))),
-      (H~+>F)(Some(Step(4,Some(C)))),
-      (H~+>C)(Some(Step(1,Some(C)))),
-      (H~+>D)(Some(Step(2,Some(C)))),
-      (H~+>H)(Some(Step(0,None))),
-      (H~+>E)(Some(Step(3,Some(C)))),
-      (E~+>B)(Some(Step(1,Some(B)))),
-      (E~+>F)(Some(Step(1,Some(F)))),
-      (E~+>C)(Some(Step(2,Some(B)))),
-      (E~+>D)(Some(Step(3,Some(B)))),
-      (E~+>H)(Some(Step(1,Some(H)))),
-      (E~+>E)(Some(Step(0,None))),
-      (A~+>B)(Some(Step(1,Some(B)))),
-      (A~+>F)(Some(Step(5,Some(B)))),
-      (A~+>C)(Some(Step(2,Some(B)))),
-      (A~+>D)(Some(Step(3,Some(B)))),
-      (A~+>H)(Some(Step(5,Some(B)))),
-      (A~+>E)(Some(Step(4,Some(B)))),
-      (A~+>A)(Some(Step(0,None)))
-    )
-
     labelGraph.edges.toOuter should be (expectedEdges2)
-    EdgeHelp.checkEdgeSets(labelGraph.edges.toOuter.asInstanceOf[Set[MLDiEdge[String]]],expectedEdges2) should be (true)
+    EdgeHelp.checkEdgeSets(labelGraph.edges.toOuter.asInstanceOf[Set[MLDiEdge[String]]],expectedEdges2) should be (None)
 
   }
 
@@ -161,8 +161,14 @@ class OneShortestPathTest extends FlatSpec with Matchers {
     val labelGraph = Dijkstra.allPairsShortestPaths(oneShortestPath,new OneShortestPathGraphBuilder[String](oneShortestPath.semiring))(graph)
 
     labelGraph.edges.toOuter should be (expectedEdges)
-//todo switch back on    EdgeHelp.checkEdgeSets(labelGraph.edges.toOuter.asInstanceOf[Set[MLDiEdge[String]]],expectedEdges) should be (true)
 
+    EdgeHelp.checkEdgeSets(labelGraph.edges.toOuter.asInstanceOf[Set[MLDiEdge[String]]],expectedEdges) match {
+      case None =>
+      case Some(firstMessage) => {
+        //try the other set of edges
+        EdgeHelp.checkEdgeSets(labelGraph.edges.toOuter.asInstanceOf[Set[MLDiEdge[String]]],expectedEdges2) should be (None)
+      }
+    }
   }
 
 }
