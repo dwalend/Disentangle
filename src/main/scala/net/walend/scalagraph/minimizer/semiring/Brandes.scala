dfb1b71bc7c9c343aaa29de9273a3ea318928aad
dwalend <david@walend.net>
2014-04-27 23:37:47 -0400

    Brandes runs Dijkstra's algorithm backwards -- more true to Brande's algorithm by avoiding an extra sort.

--- src/main/scala/net/walend/scalagraph/minimizer/semiring/Brandes.scala
+++ src/main/scala/net/walend/scalagraph/minimizer/semiring/Brandes.scala
@@ -3,7 +3,6 @@ package net.walend.scalagraph.minimizer.semiring
 import scalax.collection.Graph
 import scalax.collection.mutable.{Graph => MutableGraph}
 import net.walend.scalagraph.minimizer.heap.{FibonacciHeap, Heap}
-import scala.collection.mutable
 import scala.reflect.runtime.universe.TypeTag
 import scalax.collection.GraphPredef._
 import scala.Some
@@ -16,30 +15,56 @@ import scala.Some
  */
 object Brandes {
 
-  //todo do you really need the TypeTags ?
-  def partialBetweenness[N: TypeTag, CL, Label <: Option[Steps[N, CL]], Key](support: AllPaths[N, CL, Key])
-                                                                                      (labelGraph: Graph[N, MLDiEdge])
-                                                                                      (sink: labelGraph.NodeT): Map[N, Double] = {
+  /**
+   * Dijkstra's algorithm, run backwards.
+   */
+  def dijkstraToSink [N:Manifest,Label,Key](labelGraph:MutableGraph[N,MLDiEdge])
+                                     (sink:labelGraph.NodeT)
+                                     (support:GraphMinimizerSupport[Label,Key]):(Graph[N,MLDiEdge],Seq[labelGraph.EdgeT]) = {
+    //Set up the map of Nodes to HeapKeys
+    val heap:Heap[Key,labelGraph.NodeT] = new FibonacciHeap(support.heapOrdering)
+    import scala.collection.breakOut
+    val nodesToHeapMembers:Map[labelGraph.NodeT,heap.HeapMember] = labelGraph.nodes.map(node => (node -> heap.insert(support.heapKeyForLabel(support.semiring.O),node)))(breakOut)
 
-    //Get all the successor edges, and sort them using the heap comparator, farthest to closest
-    class EdgeOrdering extends Ordering[labelGraph.EdgeT] {
+    //Raise innerSourceNode's to I
+    nodesToHeapMembers.getOrElse(sink,throw new IllegalStateException("No HeapMember for sink "+sink)).raiseKey(support.heapKeyForLabel(support.semiring.I))
 
-      def keyForEdge(edge: labelGraph.EdgeT): Key = {
-        val label: Label = edge.label.asInstanceOf[Label]
-        support.heapKeyForLabel(label)
+    //Stack of visited nodes
+    import scala.collection.mutable.Stack
+    val stack:Stack[labelGraph.EdgeT] = Stack[labelGraph.EdgeT]()
+
+    //While the heap is not empty
+    while(!heap.isEmpty) {
+      //take the top node
+      val topNode = heap.takeTopValue()
+      topNode ~>? sink match {
+        case Some(edge) => stack.push(edge)
+        case None => //ignore
       }
 
-      //todo test this. Might need to be the opposite order
-      override def compare(x: labelGraph.type#EdgeT, y: labelGraph.type#EdgeT): Int = {
-        val xKey = keyForEdge(x)
-        val yKey = keyForEdge(y)
-        if (support.heapOrdering.lt(xKey, yKey)) -1
-        else if (support.heapOrdering.gt(xKey, yKey)) 1
-        else 0
+      //For any node that this node can reach and is not yet visited (because it's key is still in the heap)
+      for(predecessor <- topNode.diPredecessors) {
+        //if the node has not yet been visited (because its key is still in the heap)
+        val heapKey = nodesToHeapMembers.getOrElse(predecessor,throw new IllegalStateException("No HeapMember for "+predecessor))
+        if(heapKey.isInHeap) {
+          //Relax to get a new label
+          val label = support.semiring.relax(labelGraph)(predecessor,topNode,sink)
+          //Try to change the key
+          heapKey.raiseKey(support.heapKeyForLabel(label))
+        }
       }
     }
-    //todo do a variation that takes the sort in from Dijkstra's algorithm building a stack
-    val edges: Seq[labelGraph.EdgeT] = sink.incoming.to[Seq].sorted(new EdgeOrdering)
+
+    (labelGraph,stack)
+  }
+
+  //todo do you really need the TypeTags ?
+  /**
+   * Find partial betweenness
+   */
+  def partialBetweenness[N: TypeTag, CL, Label <: Option[Steps[N, CL]], Key](support: AllPaths[N, CL, Key])
+                                                                            (labelGraph: Graph[N, MLDiEdge])
+                                                                            (sink: labelGraph.NodeT,edges: Seq[labelGraph.EdgeT]): Map[N, Double] = {
 
     import scala.collection.mutable.{Map => MutableMap}
     val nodesToPartialBetweenness: MutableMap[N, Double] = MutableMap()
@@ -70,12 +95,43 @@ object Brandes {
   }
 
   /**
-   * This method finds betweenness for all nodes.
+   * Sorts internally, for when you don't get a stack from Dijkstra's algorithm or some other side effect.
+   *
+   */
+  def partialBetweennessWithSort[N: TypeTag, CL, Label <: Option[Steps[N, CL]], Key](support: AllPaths[N, CL, Key])
+                                                                            (labelGraph: Graph[N, MLDiEdge])
+                                                                            (sink: labelGraph.NodeT): Map[N, Double] = {
+
+    //Get all the successor edges, and sort them using the heap comparator, farthest to closest
+    class EdgeOrdering extends Ordering[labelGraph.EdgeT] {
+
+      def keyForEdge(edge: labelGraph.EdgeT): Key = {
+        val label: Label = edge.label.asInstanceOf[Label]
+        support.heapKeyForLabel(label)
+      }
+
+      //todo test this. Might need to be the opposite order
+      override def compare(x: labelGraph.type#EdgeT, y: labelGraph.type#EdgeT): Int = {
+        val xKey = keyForEdge(x)
+        val yKey = keyForEdge(y)
+        if (support.heapOrdering.lt(xKey, yKey)) -1
+        else if (support.heapOrdering.gt(xKey, yKey)) 1
+        else 0
+      }
+    }
+    //todo do a variation that takes the sort in from Dijkstra's algorithm building a stack
+    val edges: Seq[labelGraph.EdgeT] = sink.incoming.to[Seq].sorted(new EdgeOrdering)
+
+    partialBetweenness(support)(labelGraph)(sink,edges)
+  }
+
+  /**
+   * This method finds betweenness for all nodes, given a labelGraph with all least paths.
    */
   def betweenness[N: TypeTag, CL, Label <: Option[Steps[N, CL]], Key](support: AllPaths[N, CL, Key])
                                                                                (labelGraph: Graph[N, MLDiEdge]): Map[N, Double] = {
     val partialBetweennesses: Seq[Map[N, Double]] = for (node: labelGraph.NodeT <- labelGraph.nodes.to[Seq]) yield {
-      partialBetweenness(support)(labelGraph)(node)
+      partialBetweennessWithSort(support)(labelGraph)(node)
     }
 
     def betweennessForNode(node: N): Double = {
@@ -92,10 +148,18 @@ object Brandes {
                                   Label <: Option[Steps[N, CL]],
                                   Key]
                                   (support:AllPaths[N, CL, Key])
-                                  (originalLabelGraph:MutableGraph[N,MLDiEdge]):(Graph[N,MLDiEdge],Map[N, Double]) = {
+                                  (labelGraph:MutableGraph[N,MLDiEdge]):(Graph[N,MLDiEdge],Map[N, Double]) = {
 
-    val labelGraph: Graph[N, MLDiEdge] = Dijkstra.allPairsShortestPaths(support)(originalLabelGraph)
-    val betweennessMap = betweenness(support)(labelGraph)
+    val partialBetweennesses: Seq[Map[N, Double]] = for(sink <- labelGraph.nodes.to[Seq]) yield {
+      val labelGraphAndNodeStack = dijkstraToSink(labelGraph)(sink)(support)
+      partialBetweenness(support)(labelGraph)(sink,labelGraphAndNodeStack._2)
+    }
+
+    def betweennessForNode(node: N): Double = {
+      partialBetweennesses.map(x => x.getOrElse(node, 0.0)).sum
+    }
+    val betweennessMap:Map[N, Double] = labelGraph.nodes.map(node => (node.value, betweennessForNode(node))).toMap
+
     (labelGraph,betweennessMap)
   }
 
