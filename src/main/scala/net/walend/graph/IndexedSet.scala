7aea42bbb2992bc47fb82b343f5bb3fa611088d7
dwalend <david@walend.net>
2014-07-15 17:55:53 -0400

    Got IndexedSet to compile with many interfaces implemented

--- src/main/scala/net/walend/graph/IndexedSet.scala
+++ src/main/scala/net/walend/graph/IndexedSet.scala
@@ -1,7 +1,9 @@
 package net.walend.graph
 
-import scala.collection.generic.{ImmutableSetFactory, GenericSetTemplate}
-import scala.collection.{SetLike, AbstractSet}
+import scala.collection.generic.{GenericParTemplate, ParSetFactory, ImmutableSetFactory, GenericSetTemplate}
+import scala.collection.parallel.{ParSetLike, IterableSplitter, Combiner}
+import scala.collection.parallel.immutable.ParSet
+import scala.collection.{CustomParallelizable, SetLike, AbstractSet}
 
 /**
  *
@@ -9,9 +11,7 @@ import scala.collection.{SetLike, AbstractSet}
  * @author dwalend
  * @since v0.1.0
  */
-//todo  CustomParallelizable[A, ParIndexedSet[A]]
-//  AbstractSet[A] with Set[A] with GenericSetTemplate[A, HashSet] with SetLike[A, HashSet[A]] with CustomParallelizable[A, ParHashSet[A]] with Serializable
-class IndexedSet[A](seq:Seq[A]) extends AbstractSet[A] with Set[A] with GenericSetTemplate[A, IndexedSet] with SetLike[A, IndexedSet[A]] with Serializable  {
+class IndexedSet[A](seq:Seq[A]) extends AbstractSet[A] with Set[A] with GenericSetTemplate[A, IndexedSet] with SetLike[A, IndexedSet[A]] with CustomParallelizable[A, ParIndexedSet[A]] with Serializable  {
 
   val asSet:Set[A] = seq.to[Set]
 
@@ -42,9 +42,43 @@ class IndexedSet[A](seq:Seq[A]) extends AbstractSet[A] with Set[A] with GenericS
 
 }
 
-//extends ImmutableSetFactory[IndexedSet] with Serializable
 object IndexedSet extends ImmutableSetFactory[IndexedSet] with Serializable {
-  def apply[A](traversable:Traversable[A]) = IndexedSeq(traversable.to[Seq].distinct)
+//  def apply[A](traversable:Traversable[A]) = IndexedSeq(traversable.to[Seq].distinct)
 
-  def emptyInstance = new IndexedSet[Any](Seq.empty)
+  lazy val emptyInstance = new IndexedSet[Any](Seq.empty)
+}
+
+//extends ParSet[T] with GenericParTemplate[T, ParHashSet] with ParSetLike[T, ParHashSet[T], HashSet[T]] with Serializable
+class ParIndexedSet[A](indexedSet:IndexedSet[A]) extends GenericParTemplate[A, ParIndexedSet] with ParSet[A] with ParSetLike[A, ParIndexedSet[A], IndexedSet[A]] with Serializable {
+  //ParSet methods
+  //ParIterableLike methods
+  override def seq: IndexedSet[A] = indexedSet
+
+//  override protected[parallel] def splitter: IterableSplitter[A] = ???
+  //seq.par.splitter is protected, but iterator is not. However, iterator just returns splitter. casting.
+  override def splitter: IterableSplitter[A] = indexedSet.seq.par.iterator.asInstanceOf[IterableSplitter[A]]
+
+  //GenSetLike methods
+  override def +(elem: A): ParIndexedSet[A] = new ParIndexedSet(indexedSet + elem)
+
+  override def contains(elem: A): Boolean = indexedSet.contains(elem)
+
+  override def -(elem: A): ParIndexedSet[A] = new ParIndexedSet(indexedSet - elem)
+
+  //todo I'm surprised I had to put this here. Worked in IndexedSet without it.
+  override def empty = new ParIndexedSet[A](new IndexedSet(Seq.empty))
+
+  //GenTraversableLike
+  override def size: Int = indexedSet.size
+
+  //GenericParTemplate
+  override def companion = ParIndexedSet
+}
+
+//extends ParSetFactory[ParHashSet] with Serializable
+object ParIndexedSet extends ParSetFactory[ParIndexedSet] {
+  //ParSetFactory methods
+  override def newCombiner[A]: Combiner[A, ParIndexedSet[A]] = ???
+
+  def emptyInstance = new ParIndexedSet(IndexedSet.emptyInstance)
 }
