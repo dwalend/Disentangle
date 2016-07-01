3ade3e5cf21468cc031987ed6271396ce68a77bc
dwalend <david@walend.net>
2016-07-01 17:20:34 -0400

    Changed AgglomerateTest to be a real test

--- graph/src/test/scala/net/walend/disentangle/graph/cluster/AgglomerateTest.scala
+++ graph/src/test/scala/net/walend/disentangle/graph/cluster/AgglomerateTest.scala
@@ -2,11 +2,15 @@ package net.walend.disentangle.graph.cluster
 
 import net.walend.disentangle.graph.SomeGraph
 import net.walend.disentangle.graph.cluster.Agglomerate.ClusterGraph
+import org.scalatest.{FlatSpec, Matchers}
 
 
-object AgglomerateTest {
+class AgglomerateTest extends FlatSpec with Matchers {
 
-  val testGraph = SomeGraph.testUndigraph//todo work with the karate school graph
-  val initialCluster = Agglomerate.initialClusterFromGraph(testGraph)
-  val clusters: List[ClusterGraph] = Agglomerate.agglomerate(initialCluster)
+  "Testing with SomeGraph" should "not crash" in {
+
+    val testGraph = SomeGraph.testUndigraph //todo work with the karate school graph
+    val initialCluster = Agglomerate.initialClusterFromGraph(testGraph)
+    val clusters: List[ClusterGraph] = Agglomerate.agglomerate(initialCluster)
+  }
 }
